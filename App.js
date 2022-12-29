886eb6d01d5831e8a0a29504502c6bb8008e43a1
Rahul B <93582933+RahulBisht001@users.noreply.github.com>
2022-12-29 16:19:39 +0530

    Update App.js

--- App.js
+++ App.js
@@ -6,14 +6,13 @@ const options = {
 	},
 };
 
-getWeather = (city) => {
-	fetch(
+getWeather = async (city) => {
+	await fetch(
 		"https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city,
 		options
 	)
 		.then((response) => response.json())
 		.then((response) => {
-			// console.log(response);
 			document.getElementById(
 				"cityName"
 			).innerHTML = `Weather Chart for ${city}`;
